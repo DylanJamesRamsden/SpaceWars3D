ff4ca932565bdd2611efae03bee0b993c8869a55
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-09-27 12:53:06 +0200

    Created base logic for an enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    GameState CurrentGameState = GameState.Ready;
+
+    public float ForwardMovementSpeed = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health MyHealth = GetComponent<Health>();
+        if (!MyHealth)
+        {
+            Debug.LogError("Add health component to Enemy prefab!");
+            return;
+        }
+
+        MyHealth.OnHealthChanged += OnHealthChanged;
+        MyHealth.OnHealthDepleted += OnHealthDepleted;
+
+        GameManager.Instance.OnStateChanged += OnStateChanged;
+    }
+
+    void OnHealthChanged(int NewHealth)
+    {
+
+    }
+
+    void OnHealthDepleted()
+    {
+
+    }
+
+    void OnStateChanged(GameState NewState)
+    {
+        CurrentGameState = NewState;
+
+        if (CurrentGameState == GameState.Running)
+        {
+            StartCoroutine(Move());
+        }
+    }
+
+    protected virtual IEnumerator Move()
+    {
+        while (CurrentGameState == GameState.Running)
+        {
+            transform.position += transform.forward * ForwardMovementSpeed;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
