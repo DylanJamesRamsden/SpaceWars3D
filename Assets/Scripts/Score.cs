f672164c16cf92ff9e519b32eefd338cbc77d49c
Dylan James Ramsden <43576324+DylanJamesRamsden@users.noreply.github.com>
2023-10-01 15:15:45 +0200

    Added comments to Score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Score : MonoBehaviour
 {
 
+    // The amount of Score to give to the Player
     public int ScoreToGive = 10;
 
+    /// <summary>
+    /// Gives score to the Player.
+    /// </summary>
     public void GiveScore()
     {
         PlayerController.AddScore(ScoreToGive);
